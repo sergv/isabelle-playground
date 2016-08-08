4912243d4e5185483879cbdf1576e2b072cfa38a
Sergey Vinokurov <serg.foo@gmail.com>
2016-08-08 10:48:24 +0300

    Add Chapter 4. Solve exercise 4.1

---
+++ Chapter4.thy
@@ -0,0 +1,106 @@
+theory Chapter4
+imports Main
+begin
+
+(* Chapter 4 Logic and Proof Beyond Equality *)
+
+(* Formulas are terms of type bool.
+
+   The impliation \<Longrightarrow> is part of the Isabelle framework and should be used to structure theorems and
+   proofs.
+
+   There's also a formula for implication: \<longrightarrow>. It's part of HOL.
+
+   Both \<Longrightarrow> and \<longrightarrow> are logically equivalent, but \<Longrightarrow> should be preferred in proofs because of its
+   special treatment by Isabelle. Theorems must be of the form \<lbrakk> A\<^sub>1; A\<^sub>2; ...; A\<^sub>n \<rbrakk> \<Rightarrow> A rather
+   than A\<^sub>1 \<and> A\<^sub>2 \<and> ... \<and> A\<^sub>n \<longrightarrow> A.
+*)
+
+(* Exercise 4.1  *)
+
+datatype 'a tree =
+    Tip
+  | Node "'a" "'a tree" "'a tree"
+
+fun set :: "'a tree \<Rightarrow> 'a set" where
+"set Tip                 = {}" |
+"set (Node x left right) = {x} \<union> set left \<union> set right"
+
+datatype 'a bounds = Bounds 'a 'a 'a
+
+fun element :: "'a bounds \<Rightarrow> 'a" where
+"element (Bounds x _ _) = x"
+
+fun lower_bound :: "'a bounds \<Rightarrow> 'a" where
+"lower_bound (Bounds _ y _) = y"
+
+fun upper_bound :: "'a bounds \<Rightarrow> 'a" where
+"upper_bound (Bounds _ _ z) = z"
+
+fun tree_lower_bound :: "'a \<Rightarrow> ('a bounds) tree \<Rightarrow> 'a" where
+"tree_lower_bound x Tip          = x" |
+"tree_lower_bound _ (Node b _ _) = lower_bound b"
+
+fun tree_upper_bound :: "'a \<Rightarrow> ('a bounds) tree \<Rightarrow> 'a" where
+"tree_upper_bound x Tip          = x" |
+"tree_upper_bound _ (Node b _ _) = upper_bound b"
+
+fun annotate_with_bounds :: "'a tree \<Rightarrow> ('a bounds) tree" where
+"annotate_with_bounds Tip                 = Tip" |
+"annotate_with_bounds (Node x left right) = 
+ (let left'  = annotate_with_bounds left;
+      right' = annotate_with_bounds right
+  in  Node (Bounds x (tree_lower_bound x left') (tree_upper_bound x right')) left' right')"
+
+fun is_ordered :: "(int bounds) tree \<Rightarrow> bool" where
+"is_ordered Tip = True" |
+"is_ordered (Node b left right) = 
+ (let elem = element b in
+  is_ordered left \<and>
+  is_ordered right \<and>
+  elem \<ge> tree_upper_bound elem left \<and>
+  elem \<le> tree_lower_bound elem right)"
+
+fun ord :: "int tree \<Rightarrow> bool" where
+"ord t = is_ordered (annotate_with_bounds t)"
+
+fun ins :: "int \<Rightarrow> int tree \<Rightarrow> int tree" where
+"ins x Tip                 = Node x Tip Tip" |
+"ins x (Node y left right) =
+ (if x < y
+  then Node y (ins x left) right
+  else if x = y
+  then Node y left right
+  else Node y left (ins x right))"
+
+theorem ins_adds_element : "set (ins x t) = {x} \<union> set t"
+apply(induction t)
+apply(auto)
+done
+
+lemma ins_maintains_lower_bound :
+  "tree_lower_bound d (annotate_with_bounds t) \<ge> y \<Longrightarrow>
+   x \<ge> y \<Longrightarrow>
+   d \<ge> y \<Longrightarrow>
+   tree_lower_bound d (annotate_with_bounds (ins x t)) \<ge> y"
+apply(induction t arbitrary: d)
+apply(simp_all)
+apply(auto simp add: Let_def)
+done
+
+lemma ins_maintains_upper_bound :
+  "tree_upper_bound d (annotate_with_bounds t) \<le> y \<Longrightarrow>
+   x \<le> y \<Longrightarrow>
+   d \<le> y \<Longrightarrow>
+   tree_upper_bound d (annotate_with_bounds (ins x t)) \<le> y"
+apply(induction t arbitrary: d)
+apply(simp_all)
+apply(auto simp add: Let_def)
+done
+
+theorem ins_preserves_ordering : "ord t \<Longrightarrow> ord (ins x t)"
+apply(induction t)
+apply(auto simp add: Let_def ins_maintains_lower_bound ins_maintains_upper_bound)
+done
+
+end
